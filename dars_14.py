23bce9413b58026b7f7961fbd35e118f56e4fc56
Ezoza Hikmatillaeva <khikmatillaevaezoza@gmail.com>
2022-02-07 07:56:53 +0500

    Create dars_14.py

---
+++ dars_14.py
@@ -0,0 +1,42 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Feb  7 06:54:52 2022
+
+Dictionary 
+"""
+# otam = {'ism': 'Jamshid', 'tug_yili': 1980, 'manzil': 'Angren'}
+# print(f"Dadamlarning ismi {otam['ism']}. {otam['tug_yili']} - yili {otam['manzil']} shahrida tug'ilganlar") 
+
+# taomlar = {
+#     'dadam': 'manti', 
+#     'onam': 'osh',
+#     'Ezoza': 'mastava', 
+#     'Shahzoda': 'shashlik', 
+#     'Laziza': 'perlovka', 
+#     'Jalol': 'tok-osh'
+#     }
+# print(f"Dadamlarning sevimli taomi {taomlar['dadam']}")
+# print(f"Onamlarning sevimli taomi {taomlar['onam']}")
+# print(f"Katta singlimning sevimli taomi {taomlar['Shahzoda']}")
+
+# python = {
+#     'string': 'matn', 
+#     'integer': 'butun son',
+#     'float': 'kasr son', 
+#     'if': 'agar', 
+#     'else': 'aks holda', 
+#     'list': "ro'yxat",
+#     'tuple': "o'zgarmas ro'yxat" 
+    
+#     }
+# print(python['tuple'])
+# kalit = input("Pythonga oid so'z kiriting: ").lower()
+# # print(python.get(kalit, "Bunday so'z mavjud emas"))
+# tarjima = python[kalit]
+# if kalit == None:
+#     print("Bunday so'z mavjud emas")
+# else:
+#     print(f"{kalit} so'zining ma'nosi {tarjima} deb tarjima qilinadi")
+    
+
+
